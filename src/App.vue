9d022dbe6d9a3e703f8ecbd243817ab7723214ea
Final Web Design, Inc <finalwebdesign@gmail.com>
2021-11-22 00:37:33 -0500

    Main Navigation and Main Pagee Vues

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/certficates">Certificates</router-link> |
+    <router-link to="/portfolio">Portfolio</router-link> |
+    <router-link to="/contact">Contact</router-link>
   </div>
   <router-view />
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -16,6 +16,33 @@ const routes: Array<RouteRecordRaw> = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  {
+    path: "/certificates",
+    name: "Certificates",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Certificates.vue"),
+  },
+  {
+    path: "/portfolio",
+    name: "Portfolio",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Portfolio.vue"),
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Contact.vue"),
+  },
 ];
 
 const router = createRouter({
