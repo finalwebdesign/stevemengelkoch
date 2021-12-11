1c213c8604c5ff9f5025f87ee60ddf49a00a5047
Final Web Design, Inc <finalwebdesign@gmail.com>
2021-12-11 00:52:06 -0500

    Element Plus Install

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,11 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+
+import ElementPlus from 'element-plus'
+
+const app = createApp(App)
+app.use(ElementPlus, { size: 'small', zIndex: 3000 })
+
+
 createApp(App).use(router).mount("#app");
