72cad11f44ad0635327899372ee259383b29a4b1
Final Web Design, Inc <finalwebdesign@gmail.com>
2022-01-23 19:31:59 -0500

    Page Titles

---
+++ src/components/TitleAbout.vue
@@ -0,0 +1,29 @@
+<template>
+<el-container>
+  <h1>
+    LEARN MORE ABOUOT<br />
+    STEVE MENGELKOCH
+  </h1>
+  <a href="contact"><el-button>CONTACT STEVE NOW</el-button></a>
+  </el-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "TitlePortfolio",
+});
+</script>
+
+<style scoped>
+
+el-button {
+    font-family: "Montserrat", Sans-serif;
+    font-size: 20px;
+    font-weight: 700;
+    background-color: #BE8619;
+    padding: 20px 40px 20px 40px;
+    border-radius: 25px;
+}
+</style>
