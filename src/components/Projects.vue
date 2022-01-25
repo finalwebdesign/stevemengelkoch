4580fdc84dad57e0f84f7a6532bd13b9fd7b87b6
Final Web Design, Inc <finalwebdesign@gmail.com>
2022-01-25 17:51:29 -0500

    Portfolio Page Development

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -32,11 +32,15 @@
             authentic perfumes and fragrances available at the most affordable
             prices.
           </p>
-          <button>ExoticCarPartsUS.com</button>
-        </div>
-
+          <a
+            href="https://buyonlinefragrances.com"
+            target="_blank"
+            title="Buy Online Fragrances"
+            ><button>Visit BuyOnlineFragrances.com</button></a
+          >        
+          </div>
         <div class="project">
-          <h3>BuyOnlineFragrances.com</h3>
+          <h3>ExoticCarPartsUS.com</h3>
 
           <img
             src="../assets/portfolio/exoticcarpartsus.png"
@@ -49,9 +53,12 @@
             to set the standard for excellence in supplying parts and
             accessories worldwide and in supplying all things exotic.
           </p>
-          <button>
-            <a href="services/hosting" title="Hosting">Learn More: Hosting</a>
-          </button>
+          <a
+            href="https://exoticcarpartsus.com"
+            target="_blank"
+            title="Exotic Car Parts US"
+            ><button>Visit ExoticCarPartsUS.com</button></a
+          >      
         </div>
         <div class="project">
           <a href="PHP" title="PHP">
@@ -69,13 +76,17 @@
             social media integration. Be sure to check out the website for
             yourself at UCardiologyFellows.com.
           </p>
-          <button>UCardiologyFellows.com</button>
-        </div>
+          <a
+            href="https://ucardiologyfellows.com"
+            target="_blank"
+            title="U Cardiology Fellows"
+            ><button>Visit UCardiologyFellows.com</button></a
+          >              </div>
         <div class="project">
-          <h3>AlphaSapienLLC.com</h3>
+          <h3>USAGrowShop.com</h3>
           <img
-            src="../assets/portfolio/alphasapienllc.png"
-            title="Alpha Sapien LLC"
+            src="../assets/portfolio/usagrowstore.png"
+            title="USA Grow Shop"
           />
           <p>
             Alpha Sapien provides the highest quality designer bags and wallets
@@ -91,10 +102,10 @@
           >
         </div>
         <div class="project">
-          <h3>AlphaSapienLLC.com</h3>
+          <h3>CreateGroupLLC.com</h3>
           <img
-            src="../assets/portfolio/alphasapienllc.png"
-            title="Alpha Sapien LLC"
+            src="../assets/portfolio/creategroupllc.jpg"
+            title="Create Group LLC"
           />
           <p>
             Alpha Sapien provides the highest quality designer bags and wallets
@@ -102,11 +113,10 @@
             U.S.A. with the highest standard of materials.
           </p>
           <a
-            href="https://alphasapienllc.com"
+            href="https://creategroupllc.com"
             target="_blank"
-            title="Alpha Sapien LLC
-        "
-            ><button>Visit AlphaSapienLLC.com</button></a
+            title="Create Group LLC"
+            ><button>Visit CreateGroupLLC.com</button></a
           >
         </div>
       </div>
@@ -115,6 +125,9 @@
 </template>
 
 <style scoped>
+h3 {
+  font-size: 180%;
+}
 .projects {
   padding-bottom: 10px;
   margin-bottom: 10px;
