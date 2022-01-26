832e44cd238aa6caa38443cc6254b13baefe61b4
Final Web Design, Inc <finalwebdesign@gmail.com>
2022-01-26 14:39:46 -0500

    Portfolio Projects Development

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -30,18 +30,18 @@
           <p>
             BuyOnlineFragrances.com is a leading online distributor of 100%
             authentic perfumes and fragrances available at the most affordable
-            prices.
+            prices. Grab all your favorite fragrances products at Buy Online
+            Fragrances.
           </p>
           <a
             href="https://buyonlinefragrances.com"
             target="_blank"
             title="Buy Online Fragrances"
             ><button>Visit BuyOnlineFragrances.com</button></a
-          >        
-          </div>
+          >
+        </div>
         <div class="project">
           <h3>ExoticCarPartsUS.com</h3>
-
           <img
             src="../assets/portfolio/exoticcarpartsus.png"
             title="javascript"
@@ -49,16 +49,14 @@
           <p>
             Exotic Car Parts U.S. was created out of passion for exotics cars
             and the desire to be the industry’s best and largest independent
-            parts distributor for the international exotic community. We strive
-            to set the standard for excellence in supplying parts and
-            accessories worldwide and in supplying all things exotic.
+            parts distributor for the international exotic community.
           </p>
           <a
             href="https://exoticcarpartsus.com"
             target="_blank"
             title="Exotic Car Parts US"
             ><button>Visit ExoticCarPartsUS.com</button></a
-          >      
+          >
         </div>
         <div class="project">
           <a href="PHP" title="PHP">
@@ -69,19 +67,18 @@
               title="UCardiologyFellows.com"
           /></a>
           <p>
-            Final Web Design is proud to present another new website design and
-            developed for our friends over at the University of Miami Cardiology
-            Fellowship. This website is built on the WordPress CMS and includes
-            features such as a JavaScript cardiology calculator, blog feeds and
-            social media integration. Be sure to check out the website for
-            yourself at UCardiologyFellows.com.
+            Our friends over at the University of Miami Cardiology Fellowship
+            are enjoying their new website on the WordPress CMS & includes
+            features such as a JavaScript cardiology calculator, blog feeds &
+            social media integration.
           </p>
           <a
             href="https://ucardiologyfellows.com"
             target="_blank"
             title="U Cardiology Fellows"
             ><button>Visit UCardiologyFellows.com</button></a
-          >              </div>
+          >
+        </div>
         <div class="project">
           <h3>USAGrowShop.com</h3>
           <img
@@ -116,6 +113,62 @@
             href="https://creategroupllc.com"
             target="_blank"
             title="Create Group LLC"
+            ><button>Visit CreateGroupLLC.com</button></a
+          >
+        </div>
+        <div class="project">
+          <h3>HennepinTech.edu</h3>
+          <img
+            src="../assets/portfolio/hennepintech.jpg"
+            title="Hennepin Tech College"
+          />
+          <p>
+            Over the years, HTC’s course offerings and degree programs have
+            evolved, adapted, and changed to reflect the needs of employers and
+            of today’s students. HTC will continue keeping its eye on the
+            future, and will continue our history of innovation for many years
+            to come.
+          </p>
+          <a
+            href="https://hennepintech.edu/"
+            target="_blank"
+            title="Hennepin Tech College"
+            ><button>Visit HennepinTech.edu</button></a
+          >
+        </div>
+        <div class="project">
+          <h3>Coder Clothing</h3>
+          <img
+            src="../assets/portfolio/coder-clothing.jpg"
+            title="Create Group LLC"
+          />
+          <p>
+            Alpha Sapien provides the highest quality designer bags and wallets
+            available. All of our products are designed in Italy, made in the
+            U.S.A. with the highest standard of materials.
+          </p>
+          <a
+            href="https://creategroupllc.com"
+            target="_blank"
+            title="Create Group LLC"
+            ><button>Visit CreateGroupLLC.com</button></a
+          >
+        </div>
+        <div class="project">
+          <h3>DJsMiamiFL.com</h3>
+          <img
+            src="../assets/portfolio/djsmiamifl.jpg"
+            title="DJs Miami FL"
+          />
+          <p>
+            Alpha Sapien provides the highest quality designer bags and wallets
+            available. All of our products are designed in Italy, made in the
+            U.S.A. with the highest standard of materials.
+          </p>
+          <a
+            href="https://creategroupllc.com"
+            target="_blank"
+            title="DJs Miami FL"
             ><button>Visit CreateGroupLLC.com</button></a
           >
         </div>
